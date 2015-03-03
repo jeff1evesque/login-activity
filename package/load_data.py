5206617fb26d1dfa555ab960940519036a948c4e
Jeff Levesque <jeff1evesque@yahoo.com>
2015-03-03 14:34:00 -0500

    #28: load_data.py, 'import APP_ROOT'

--- package/load_data.py
+++ package/load_data.py
@@ -3,6 +3,7 @@
 ## @load_data.py
 #  This file delegates parser, validator, report generator logic, and
 #      returns the correspond login-activity report.
+from settings import APP_ROOT
 
 ## Class: Load_Data, explicitly inherit 'new-style' class
 class Load_Data(object):
