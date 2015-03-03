7e0425783e3ee309989d9612b9e024a9b14c8a3c
Jeff Levesque <jeff1evesque@yahoo.com>
2015-03-03 12:17:26 -0500

    #40: settings_report.py, remove file, obsolete

--- package/settings/settings_report.py
+++
@@ -1,4 +0,0 @@
-## @settings_report.py
-#  This file contains the class variables, used to define the directory location
-#      of the JSON dataset. The JSON dataset is used to generate the login-activity
-#      report.
