53cc2ba4b749f0f6587318846b58875fd535080f
Jeff Levesque <jeff1evesque@yahoo.com>
2015-03-03 15:00:08 -0500

    #28: load_data.py, add 'import' statements

--- package/load_data.py
+++ package/load_data.py
@@ -5,6 +5,9 @@
 #      returns the correspond login-activity report.
 import os
 from settings import LOG_ACTIVITY
+from package.parser.parser_data import Parse_Data
+from package.validator.validator_data import Validate_Data
+from package.generator.generator_data import Generate_Data
 
 ## process_report:
 def process_report():
