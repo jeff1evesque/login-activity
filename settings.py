ed00d0cb602593e53853af6128d9e8c3126060f1
Jeff Levesque <jeff1evesque@yahoo.com>
2015-03-03 14:23:41 -0500

    #56: settings.py, adjust docblock

--- settings.py
+++ settings.py
@@ -2,6 +2,8 @@
 
 ## @settings.py
 #  This file contains various setting variables
+#
+#  Note:  __file__ refers to the current file (settings.py)
 import os
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
