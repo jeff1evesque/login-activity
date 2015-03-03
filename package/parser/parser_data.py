3edc57d049a3c4caafeb96537474e5d8df621190
Jeff Levesque <jeff1evesque@yahoo.com>
2015-03-03 16:52:36 -0500

    #27: parser_data.py, adjust docblock

--- package/parser/parser_data.py
+++ package/parser/parser_data.py
@@ -2,3 +2,7 @@
 
 ## @parser_data.py
 #  This file parses the supplied file-object into a python dictionary.
+#
+#  Note: during each iteration of parsing the supplied file-object, the
+#        sub-dataset is validated against an imported jsonschema, then
+#        appended to an overall restructured data.
