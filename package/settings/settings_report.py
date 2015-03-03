5bca70d15f9ebcbf4423442172b5c510862e77eb
Jeff Levesque <jeff1evesque@yahoo.com>
2015-03-03 11:54:42 -0500

    #40: settings_report.py, add file with docblock

---
+++ package/settings/settings_report.py
@@ -0,0 +1,4 @@
+## @settings_report.py
+#  This file contains the class variables, used to define the directory location
+#      of the JSON dataset. The JSON dataset is used to generate the login-activity
+#      report.
