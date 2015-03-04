357c076fbea40dc73139beb5ee34e81fd2f8495d
Jeff Levesque <jeff1evesque@yahoo.com>
2015-03-04 17:41:14 -0500

    #33: jsonschema_metrics.py, define 'jsonschema_metrics'

--- package/schema/jsonschema_metrics.py
+++ package/schema/jsonschema_metrics.py
@@ -2,3 +2,72 @@
 
 ## @jsonschema_metrics.py
 #  This file contains the valid schema for the login metrics.
+
+## jsonschema_metrics: returns the schema used to validate the login metric for
+#                      each user within the provided dataset.
+def jsonschema_metrics():
+  schema = {
+    'type': 'object',
+    'properties': {
+      'email': {
+        'type': 'string',
+        #'format': 'email',
+        'minLength': 1
+      },
+      'login_success': {
+        'type': 'array',
+        'minItems': 1,
+        'items': {
+          'type': 'string',
+          'format': 'date-time',
+          'minLength': 1
+        },
+      },
+      'login_failure': {
+        'type': 'array',
+        'minItems': 1,
+        'items': {
+          'type': 'string',
+          'format': 'date-time',
+          'minLength': 1
+        },
+      },
+      'logout_success': {
+        'type': 'array',
+        'minItems': 1,
+        'items': {
+          'type': 'string',
+          'format': 'date-time',
+          'minLength': 1
+        },
+      },
+      'back30days': {
+        'enum': [True, False]
+      },
+      'back60days': {
+        'enum': [True, False]
+      },
+      'back90days': {
+        'enum': [True, False]
+      },
+      'count_success': {
+        'type': 'integer',
+        'minLength': 1
+      },
+      'count_failure': {
+        'type': 'integer',
+        'minLength': 1
+      },
+      'login_first': {
+        'type': 'string',
+        'format': 'date-time',
+        'minLength': 1
+      },
+      'login_last': {
+        'type': ['string', None],
+        'format': 'date-time',
+        'minLength': 1
+      },
+    },
+  }
+return schema
