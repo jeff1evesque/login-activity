eca447b78531365d83bf335a79fd0075f83382a9
Jeff Levesque <jeff1evesque@yahoo.com>
2015-03-04 16:31:52 -0500

    #29: validator_data.py, define class constructor

--- package/validator/validator_data.py
+++ package/validator/validator_data.py
@@ -2,3 +2,10 @@
 
 ## @validator_data.py
 #  This file performs various validations on login data.
+
+## Class: Validate_Data, explicitly inherit 'new-style' class
+class Validate_Data(object):
+
+  ## constructor:
+  def __init__(self, data):
+    self.data = data
