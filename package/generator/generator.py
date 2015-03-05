f21ec5157da0a862b125785132ed6ff0598f90ee
Jeff Levesque <jeff1evesque@yahoo.com>
2015-03-04 23:32:27 -0500

    #30: generator.py, define class constructor

--- package/generator/generator.py
+++ package/generator/generator.py
@@ -3,3 +3,10 @@
 ## @generator.py
 #  This file generates a report of user login-activity using the supplied
 #      dataset.
+
+## Class: Generate_Data, explicitly inherit 'new-style' class
+class Generate_Data(object):
+
+  ## constructor:
+  def __init__(self, user_metrics):
+    self.user_metrics = user_metrics
