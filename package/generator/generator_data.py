170b1a70ab258ec716cb4ab68a3bc54639c933d9
Jeff Levesque <jeff1evesque@yahoo.com>
2015-03-04 23:37:22 -0500

    #30: generator_data.py, stub methods

--- package/generator/generator_data.py
+++ package/generator/generator_data.py
@@ -10,3 +10,9 @@ class Generate_Data(object):
   ## constructor:
   def __init__(self, user_metrics):
     self.user_metrics = user_metrics
+
+  ## generate_report: generate the login-activity report displayed on a webpage.
+  def generate_report(self):
+
+  ## generate_csv: generate the login-activity report contained within a csv.
+  def generate_csv(self):
