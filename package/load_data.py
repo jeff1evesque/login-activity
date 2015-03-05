ccb61f769a82f85149c679806b5edf1b563768b7
Jeff Levesque <jeff1evesque@yahoo.com>
2015-03-04 22:46:06 -0500

    #90: load_data.py, remove 'import os'

--- package/load_data.py
+++ package/load_data.py
@@ -3,7 +3,6 @@
 ## @load_data.py
 #  This file delegates parser, validator, report generator logic, and
 #      returns the correspond login-activity report.
-import os
 from settings import LOG_ACTIVITY
 from package.parser.parser_data import Parse_Data
 from package.generator.generator_data import Generate_Data
